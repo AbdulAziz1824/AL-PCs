b3075eb26655584f8445bc0fdc5353fb48819e3f
Abo_omran <136702051+AbdulAziz1824@users.noreply.github.com>
2026-08-27 02:02:56 +0300

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>AL-PCs</h1>
+      <p>ابنِ تجميعة الكمبيوتر الخاصة بك</p>
+
+      <button>ابدأ بناء جهازك</button>
+    </main>
+  );
+}
