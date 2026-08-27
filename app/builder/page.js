dbe5bd97924996d1c632f3f279b3531e1e47b4de
Abo_omran <136702051+AbdulAziz1824@users.noreply.github.com>
2026-08-27 14:16:19 +0300

    Update page.js

--- app/builder/page.js
+++ app/builder/page.js
@@ -4,38 +4,19 @@ export default function Home() {
       style={{
         minHeight: "100vh",
         background: "#050505",
-        color: "white",
+        color: "#39ff88",
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
-        textAlign: "center",
         fontFamily: "Arial",
+        textAlign: "center",
       }}
     >
       <div>
-        <h1 style={{ fontSize: "60px", color: "#39ff88" }}>
-          AL PC
-        </h1>
-
-        <p style={{ fontSize: "20px", color: "#aaa" }}>
-          منصة تجميع أجهزة الكمبيوتر
+        <h1>AL PC</h1>
+        <p style={{ color: "#aaa" }}>
+          الموقع يعمل بنجاح
         </p>
-
-        <button
-          style={{
-            marginTop: "20px",
-            padding: "14px 30px",
-            border: "none",
-            borderRadius: "10px",
-            background: "#39ff88",
-            color: "#000",
-            fontWeight: "bold",
-            fontSize: "16px",
-            cursor: "pointer",
-          }}
-        >
-          ابدأ تجميع جهازك
-        </button>
       </div>
     </main>
   );
