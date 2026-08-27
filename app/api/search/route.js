ac02ab88cce0f92bf84f69ffd68a41b1a5a8250b
Abo_omran <136702051+AbdulAziz1824@users.noreply.github.com>
2026-08-28 02:37:42 +0300

    Update route.js

--- app/api/search/route.js
+++ app/api/search/route.js
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
-import { products } from "../../../../lib/products";
+import { products } from "@/lib/products";
+
 
 export async function GET(request) {
   const { searchParams } = new URL(request.url);
